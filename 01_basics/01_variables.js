ff94c1adab523c90169fb66f31429f9c11d959b8
Sumit kumar Gupta <158473782+skgupta05@users.noreply.github.com>
2024-11-07 18:26:16 +0000

    vairables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 12345
+let accountEmail = "skgupta@gmail.com"
+var accountPassword = "321098"
+accountCity = "jaipur"
+accountState 
+
+
+// prefer not to use var 
+// because of issu in block scope and functional scope
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
